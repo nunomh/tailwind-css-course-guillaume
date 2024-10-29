88f2545272b49f03f76160217dec72b6ef98597c
Nuno Henriques <nuno.henriques@izertislabs.pt>
2024-10-29 18:02:04 +0000

    Grids

--- app/page.tsx
+++ app/page.tsx
@@ -29,10 +29,62 @@ export default function Home() {
 
       {/* spacing */}
       <p className="mt-4">Spacing</p>
-      <div>
-        <div className="bg-blue-500">div1</div>
-        <div className="bg-red-300">div2</div>
+      <div className="flex w-full">
+        <div className="w-2/5 h-16 bg-blue-500">div1</div>
+        <div className="w-3/5 h-16 bg-red-300">div2</div>
       </div>
+
+      <div className="mt-3 Parent">
+        <div className="">Header</div>
+        <div className="flex flex-row">
+          <div className="bg-blue-500">Main content</div>
+          <div className="flex flex-col">
+            <div className="bg-red-300">Sidebar</div>
+            <div className="bg-yellow-300">Menu</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-3 Parent">
+        <div className="">Header 2</div>
+        <div className="flex flex-col">
+          <div className="flex w-full flex-nowrap">
+            <div className="w-full grow">01</div>
+            <div className="w-full grow">02</div>
+            <div className="w-full grow">03</div>
+            <div className="w-full grow">04</div>
+            <div className="w-full grow">05</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-3 Parent">
+        <div className="">Header 3</div>
+        <div className="flex flex-col">
+          <div className="flex w-full flex-wrap">
+            <div className="w-32 grow">01</div>
+            <div className="w-32 grow">02</div>
+            <div className="w-32 grow">03</div>
+            <div className="w-32 grow">04</div>
+            <div className="w-32 grow">05</div>
+          </div>
+        </div>
+      </div>
+
+      {/* Grids */}
+      <p className="mt-4">Grids</p>
+      <div className="Parent">
+        <div className="grid lg:grid-cols-4 gap-4 md:grid-cols-2">
+          <div className="p-6 rounded-lg bg-blue-500">01</div>
+          <div className="p-6 rounded-lg bg-blue-500">02</div>
+          <div className="p-6 rounded-lg bg-blue-500">03</div>
+          <div className="p-6 rounded-lg bg-blue-500">04</div>
+          <div className="p-6 rounded-lg bg-blue-500">05</div>
+          <div className="p-6 rounded-lg bg-blue-500">06</div>
+          <div className="p-6 rounded-lg bg-blue-500">07</div>
+        </div>
+      </div>
+
 
     </div>
   );
